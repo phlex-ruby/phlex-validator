19deb6fb7504da6e88949e33b7ece604374f01d1
Joel Drapper <j.drapper@plane.com>
2025-04-18 20:25:20 +0100

    Update validator.rb

--- lib/phlex/validator.rb
+++ lib/phlex/validator.rb
@@ -4,7 +4,6 @@ require "zeitwerk"
 require "phlex"
 require "literal"
 require "uri"
-require "phlex/validator/version"
 
 module Phlex::Validator
 	Loader = Zeitwerk::Loader.for_gem_extension(Phlex).tap do |loader|
